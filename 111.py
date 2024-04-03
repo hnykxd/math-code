8ba0b9c55e760fede353ec433a3edebf23c7fc03
hnykxd <120177280+hnykxd@users.noreply.github.com>
2024-04-03 22:43:17 +0800

    Add files via upload

---
+++ 111.py
@@ -0,0 +1,18 @@
+import os
+import schedule
+import time
+
+def run_file():
+    file_path = r"D:\Desktop\接下来进入到最后的宣誓阶段：.ppsx"
+    if os.path.exists(file_path):
+        os.startfile(file_path)
+    else:
+        print("文件路径不存在或者无法找到。请检查路径是否正确。")
+
+# 将run_file()函数安排在早上六点49分运行
+schedule.every().day.at("06:46:30").do(run_file)
+
+# 在程序中执行调度
+while True:
+    schedule.run_pending()
+    time.sleep(1)
